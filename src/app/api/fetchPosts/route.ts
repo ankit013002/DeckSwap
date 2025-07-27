c92620e55d0235f9c31cb9673829ce78c54a06ee
ankit013002 <112412889+ankit013002@users.noreply.github.com>
2025-07-27 12:03:52 -0400

    Users will not see their own cards in the mainpage or when doing a query.

--- src/app/api/fetchPosts/route.ts
+++ src/app/api/fetchPosts/route.ts
@@ -1,12 +1,52 @@
-import { eq, like } from "drizzle-orm";
+import { eq, like, ne, sql } from "drizzle-orm";
 import { NextResponse } from "next/server";
 import { db } from "~/server/db";
 import { items, users } from "~/server/db/schema";
+import { GetUser } from "~/server/getUser";
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const query = searchParams.get("query")?.toLowerCase() ?? "";
   const itemType = searchParams.get("itemType")?.toLocaleLowerCase() ?? "";
+  const currUser = await GetUser();
+
+  if (!currUser) {
+    console.log("HERE");
+    let rawData = null;
+    rawData = await db
+      .select({
+        id: items.id,
+        userId: items.userId,
+        title: items.title,
+        price: items.price,
+        description: items.description,
+        category: items.category,
+        condition: items.condition,
+        imageUrl: items.imageUrl,
+        soldBy: users.name,
+      })
+      .from(items)
+      .leftJoin(users, eq(items.userId, users.id));
+
+    const data = await Promise.all(
+      rawData.map(async (row) => {
+        return {
+          ...row,
+          id: row.id.toString(),
+          soldby: row.soldBy,
+          userId: row.userId.toString(),
+          title: row.title.toString(),
+          price: row.price.toString(),
+          description: row.description?.toString(),
+          category: row.category.toString(),
+          condition: row.condition?.toString(),
+          imageUrl: row.imageUrl?.toString(),
+        };
+      }),
+    );
+
+    return NextResponse.json(data);
+  }
 
   if (query || itemType) {
     try {
@@ -26,7 +66,9 @@ export async function GET(request: Request) {
           })
           .from(items)
           .leftJoin(users, eq(items.userId, users.id))
-          .where(like(items.title, `%${query}%`));
+          .where(
+            sql`${items.title} LIKE ${`%${query}%`} AND ${items.userId} <> ${currUser.id}`,
+          );
       } else {
         rawData = await db
           .select({
@@ -42,7 +84,9 @@ export async function GET(request: Request) {
           })
           .from(items)
           .leftJoin(users, eq(items.userId, users.id))
-          .where(like(items.category, `%${itemType}%`));
+          .where(
+            sql`${items.category} LIKE ${`%${itemType}%`} AND ${items.userId} <> ${currUser.id}`,
+          );
       }
 
       const data = await Promise.all(
@@ -95,7 +139,8 @@ export async function GET(request: Request) {
           soldBy: users.name,
         })
         .from(items)
-        .leftJoin(users, eq(items.userId, users.id));
+        .leftJoin(users, eq(items.userId, users.id))
+        .where(ne(items.userId, currUser?.id));
 
       const data = await Promise.all(
         rawData.map(async (row) => {
